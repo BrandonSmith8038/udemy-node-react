a9d78920b04c2fae045492dc841b1db2a38eb434
Brandon <brandon@reddirtwebdesign.com>
2018-03-29 09:42:20 +0000

    feat: Add New User To Database
    If user does not already exist add them to the database

--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,27 @@
-const passport = require("passport")
-const GoogleStrategy = require("passport-google-oauth20")
-const keys = require("../config/keys")
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
+const keys = require('../config/keys');
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-  console.log('Access Token',accessToken)
-  console.log('Refresh Token', refreshToken)
-  console.log('Profile', profile)
-}))
+const User = mongoose.model('users')
+
+passport.use(new GoogleStrategy(
+	{
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback',
+	proxy: true
+}, 
+	(accessToken, refreshToken, profile, done) => {
+			
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
+					if(existingUser){
+						
+					} else {
+							new User({ googleId: profile.id }).save()
+					}
+				})
+		}
+	)
+)

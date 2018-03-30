ae1d1c770775023117c3e87b812b2fbad91ba063
Brandon <brandon@reddirtwebdesign.com>
2018-03-30 10:46:16 +0000

    feat: Materialize Css and Header
    Added Materialize CSS and Initial Header Component

---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+  render(){
+    return (
+     <nav>
+      <div className="nav-wrapper">
+        <a className="left brand-logo">
+          EMAILY
+        </a>
+        <ul class="right">
+          <li>
+            <a>
+              Login With Google
+            </a>
+          </li>
+        </ul>
+      </div>  
+     </nav>
+    )
+  }
+}
+
+export default Header

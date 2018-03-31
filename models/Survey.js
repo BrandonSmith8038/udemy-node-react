5e9c6e48da9bded74b429530888a063e99ac06ba
Brandon <brandon@reddirtwebdesign.com>
2018-03-31 11:02:47 +0000

    feat: Survey and Recipient Schema Setup

---
+++ models/Survey.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+const RecipientSchema = require("./Recipient")
+
+const surveySchema = new Schema({
+  title:{
+    type: String
+  },
+  body:{
+    type: String
+  },
+  subject:{
+    type: String
+  },
+  recipients:{
+    type: [RecipientSchema]
+  },
+  yes: {
+    type: Number,
+    default: 0
+  },
+  no: {
+    type: Number,
+    default: 0
+  }
+})
+
+mongoose.model('surveys', surveySchema)

4849ddc9c5c04e91d55f6d96290e7fc191e5596c
Brandon <brandon@reddirtwebdesign.com>
2018-04-01 08:38:56 +0000

    feat: Redux Form Setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions'
 
 import Header from './Header'
 import Landing from './Landing'
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>Survey New</h2>
+import Dashboard from './Dashboard'
+import SurveyNew from './surveys/SurveyNew'
 
 class App extends Component {
   componentDidMount(){
@@ -21,7 +21,7 @@ class App extends Component {
             <Header />
             <Route path="/" component={Landing} exact/>
             <Route path="/surveys" component={Dashboard} exact />
-            <Route path="/surverys/new" component={SurveyNew} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>

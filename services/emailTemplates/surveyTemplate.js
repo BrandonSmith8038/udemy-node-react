e4af4997c5db7b2d2a2607acd718e00688aafa74
Brandon <brandon@reddirtwebdesign.com>
2018-04-01 07:28:30 +0000

    feat: Redirect User On Email Click

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const keys = require('../../config/keys')
+
 module.exports = (survey) => {
   return `
     <html>
@@ -10,11 +12,11 @@ module.exports = (survey) => {
           <p>
             ${survey.body}
           </p>
-          <div>
-            <a href="http://localhost:8080">Yes</a>
+          <div style="margin-bottom: 20px;">
+            <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
           </div>
           <div>
-            <a href="http://localhost:8080">No</a>
+            <a href="${keys.redirectDomain}"/api/surveys/thanks>No</a>
           </div>
         </div>
       </body>

00537221e0b34bf4dae685e2abab292633bb6221
Brandon <brandon@reddirtwebdesign.com>
2018-04-01 11:47:37 +0000

    feat: Survery Review Back Button

--- client/src/components/surveys/SurveyReview.js
+++ client/src/components/surveys/SurveyReview.js
@@ -1,9 +1,17 @@
+// Shows users their form inputs for review
+
 import React from 'react'
 
-const SurveyReview = () => {
+const SurveyReview = ({ onCancel }) => {
   return(
     <div>
       <h5>Please Review Your Entries</h5>
+      <button
+        className="yellow darken-3 btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
     </div>
   )
 }

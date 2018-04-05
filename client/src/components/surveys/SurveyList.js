bd03816a705110c7f73304cde4408dd2b0593b2d
Brandon <brandon@reddirtwebdesign.com>
2018-04-05 09:36:01 +0000

    feat: Render Cards Showing Users Surveys, Fixed Reducer Errors

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,24 +1,46 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchSurveys } from '../../actions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
 
 class SurveyList extends Component {
-  
-  componentDidMount(){
-    this.props.fetchSurveys()
+  componentDidMount() {
+    this.props.fetchSurveys();
   }
-  render(){
-    return(
+  
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card blue-grey white-text" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+              {survey.body}
+            </p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+  
+  render() {
+    return (
       <div>
-        SurveyList
+        {this.renderSurveys()}
       </div>
-    )
+    );
   }
   
 }
 
-function mapStateToProps({ surveys }){
-  return{ surveys }
+function mapStateToProps({ surveys }) {
+  return { surveys };
 }
 
-export default connect(mapStateToProps, { fetchSurveys })(SurveyList)
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);

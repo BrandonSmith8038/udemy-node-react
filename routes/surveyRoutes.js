7965a536873c72dc2138aca284bd3fc06dd68902
Brandon <brandon@reddirtwebdesign.com>
2018-04-05 08:05:45 +0000

    feat: Finished writing query to collect user feedback

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks For Voting');
   });
 
@@ -63,7 +63,11 @@ module.exports = app => {
             _id: surveyId,
             recipients: { $elemMatch: { email: email, responded: false } }
           },
-          { $inc: { [choice]: 1 }, $set: { 'recipients.$.responded': true } }
+          { 
+            $inc: { [choice]: 1 }, 
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
         ).exec();
       })
       .value();
